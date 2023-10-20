f04e0ba7bdc933c25c43d96fff2146b781d0af12
Gyeolee <gyeoleeeeeee@gmail.com>
2023-10-20 13:14:44 +0900

    [ADD] 프로젝트 생성

---
+++ CallKitSample/CallKitSample/CallKitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallKitSampleApp.swift
+//  CallKitSample
+//
+//  Created by Hangyeol on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct CallKitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
